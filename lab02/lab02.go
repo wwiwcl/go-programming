659e93722fd48616748823b488132ce5609a8a46
wwiwcl <wujinlin920315@gmail.com>
2023-10-12 15:37:03 +0800

    lab02

--- lab02/lab02.go
+++ lab02/lab02.go
@@ -21,16 +21,28 @@ func Sum(n int64) string {
 	ans = 0
 	var str string = ""
 	var i int64 = 0
-	for i = 0; i < n; i++ {
-		if i%7 != 0 {
-			ans += i
-			str += strconv.FormatInt(i, 10)
-			str += "+"
-		}
-	}
 	if n%7 != 0 {
+		for i = 0; i < n; i++ {
+			if i%7 != 0 {
+				ans += i
+				str += strconv.FormatInt(i, 10)
+				str += "+"
+			}
+		}
 		ans += n
 		str += strconv.FormatInt(n, 10)
+		str += "="
+		str += strconv.FormatInt(ans, 10)
+	} else {
+		for i = 0; i < n-1; i++ {
+			if i%7 != 0 {
+				ans += i
+				str += strconv.FormatInt(i, 10)
+				str += "+"
+			}
+		}
+		ans += (n - 1)
+		str += strconv.FormatInt(n-1, 10)
 		str += "="
 		str += strconv.FormatInt(ans, 10)
 	}
