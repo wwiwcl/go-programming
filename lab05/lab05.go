9bc0879d2a1606ed62da5c3db3a1c5a9e0d7264b
wwiwcl <wujinlin920315@gmail.com>
2023-11-03 19:38:55 +0800

    lab05

--- lab05/lab05.go
+++ lab05/lab05.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 // TODO: Please create a struct to include the information of a video
@@ -13,6 +16,12 @@ func YouTubePage(w http.ResponseWriter, r *http.Request) {
 	// TODO: Get video information from YouTube API
 	// TODO: Parse the JSON response and store the information into a struct
 	// TODO: Display the information in an HTML page through `template`
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	value := os.Getenv("YOUTUBE_API_KEY")
+	fmt.Fprintf(w, value)
 }
 
 func main() {
