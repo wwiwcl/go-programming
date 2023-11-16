341b7ba072a0b0ade46f75e5d6156b94ede23f85
wwiwcl <wujinlin920315@gmail.com>
2023-11-16 15:37:16 +0800

    lab06

--- lab06/wasm/main.go
+++ lab06/wasm/main.go
@@ -8,11 +8,26 @@ import (
 )
 
 func CheckPrime(this js.Value, args []js.Value) interface{} {
-	// TODO: Check if the number is prime
+	if len(args) != 1 {
+		return "Invalid number of arguments. Expected 1 argument"
+	}
+
+	number := args[0].String()
+	n, err := strconv.Atoi(number)
+	if err != nil {
+		return "Invalid input. Please provide a valid number"
+	}
+
+	bigInt := big.NewInt(int64(n))
+
+	isPrime := bigInt.ProbablyPrime(20) // Adjust the Miller-Rabin iterations for higher certainty
+
+	return isPrime
 }
 
 func registerCallbacks() {
 	// TODO: Register the function CheckPrime
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
 }
 
 func main() {
